8369433a581dce777b7121805d98521a6c691855
chenwentao <chenwentao1021@gmail.com>
2024-03-09 21:41:54 +0800

    提交项目所有代码

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/root/chat/src/server/chatserver.cpp" "/root/chat/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/root/chat/src/server/chatservice.cpp" "/root/chat/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/root/chat/src/server/db/db.cpp" "/root/chat/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/root/chat/src/server/main.cpp" "/root/chat/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/root/chat/src/server/model/friendmodel.cpp" "/root/chat/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "/root/chat/src/server/model/groupmodel.cpp" "/root/chat/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/root/chat/src/server/model/offlinemessagemodel.cpp" "/root/chat/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/root/chat/src/server/model/usermodel.cpp" "/root/chat/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/root/chat/src/server/redis/redis.cpp" "/root/chat/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
